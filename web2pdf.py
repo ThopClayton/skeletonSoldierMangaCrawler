3a5badd4c5992e0186f6221904ccee03cbe83961
ThopClayton <60019875+ThopClayton@users.noreply.github.com>
2023-12-20 12:16:03 -0700

    updated web crawler to handle unexpected naming conventions
    mostly the fact that chapter 141 gets a unique URL, and that chapter 167 starts with 02.jpg, yet still has a reference to 01.jpg in the web page code causing the crawler to try to download a file that doesn't exist

--- web2pdf.py
+++ web2pdf.py
@@ -3,13 +3,18 @@ import requests
 from bs4 import BeautifulSoup
 from PIL import Image
 import os
+import numpy as np
 
 
 
 def downloadPage(site, chapter):
+    '''gets all iamges form web page that are named via only numerical characters
+    all photos that have the same width as the majority of photos are combined into one PDF file
+    photos are then deleted'''
     # get images from web page, stolen from https://stackoverflow.com/a/46143318/20375434
     response = requests.get(site)
     imgNum = 0
+    imageWidths = {} # names of each image as key, width as data
 
     soup = BeautifulSoup(response.text, 'html.parser')
     img_tags = soup.find_all('img')
@@ -18,7 +23,7 @@ def downloadPage(site, chapter):
 
     
     for url in urls:
-        filename = re.search(r'/([0-9]+[.](jpg|gif|png))$', url) # regex changed to match only numerical image names to avoid other bullshit on page
+        filename = re.search(r'/([0-9]+[.](jpg|png))$', url) # regex changed to match only numerical image names to avoid other bullshit on page
         if not filename:
             # print("Regex didn't match with the url: {}".format(url))
             continue
@@ -29,28 +34,45 @@ def downloadPage(site, chapter):
                 # to be the site variable atm. 
                 url = '{}{}'.format(site, url)
             response = requests.get(url)
-            f.write(response.content)
-            imgNum += 1
+            f.write(response.content) 
+            try: # added because on chapter 167, there is a reference to a '01.jpg' even though the manga starts on 02
+                imageWidths[filename.group(1)] = Image.open(os.path.join('processing', filename.group(1))).width
+                imgNum += 1
+            except:
+                pass
 
 
     # combine images into PDF, stolen from https://stackoverflow.com/a/63574364/20375434
     imagelist = []
-    im1 = Image.open(os.path.join('processing', '2.jpg')).convert('RGB') # base for everything else. 1.jpg not part of manga for all chapters but the first, oh well.
-    for i in range(3, imgNum): # don't care about imgNum.jpg because it appears to be a note by the translator
-        image = Image.open(os.path.join('processing', f'{i}.jpg'))
+    im1 = 0 # base for everything else
+    
+    for key in imageWidths.keys():
+        if imageWidths[key] != np.median([x for x in imageWidths.values()]): # filter out images that are different widths, because they are usually extras from third parties
+            continue
+
+        image = Image.open(os.path.join('processing', key))
         im = image.convert('RGB')
-        imagelist.append(im)
+
+        if im1 == 0:
+            im1 = im
+        else:
+            imagelist.append(im)
 
     im1.save(os.path.join('chapters', f'chapter_{chapter}.pdf'), save_all=True, append_images=imagelist, quality=95)
-    #os.system('rm *.jpg')
-    for i in range(1, imgNum+1):
-        os.remove(os.path.join('processing', f'{i}.jpg'))
 
-def downloadEveryChapter():
-    chapter = 1
+    for key in imageWidths.keys():
+        os.remove(os.path.join('processing', key))
+
+def downloadEveryChapter(chapter=1):
+    '''enter the chapter you would like to start downloading from
+    can't do it for when the URL format is changed for seemingly no reason, like chapter 141'''
     website = f'https://skeleton-soldier.online/manga/skeleton-soldier-couldnt-protect-the-dungeon-chapter-1-{chapter}/'
     while(200 == requests.head(website).status_code): # true if website exists (and isn't a redirection)
-        website = f'https://skeleton-soldier.online/manga/skeleton-soldier-couldnt-protect-the-dungeon-chapter-1-{chapter}/'
+        print(f'downloading chapter: {chapter}')
+        if chapter == 141:
+            website = 'https://skeleton-soldier.online/manga/skeleton-soldier-couldnt-protect-the-dungeon-chapter-1-141-end-of-season-2/'
+        else:
+            website = f'https://skeleton-soldier.online/manga/skeleton-soldier-couldnt-protect-the-dungeon-chapter-1-{chapter}/'
         downloadPage(website, chapter)
         chapter += 1
         
@@ -59,7 +81,7 @@ def downloadEveryChapter():
 
 def main():
     #downloadPage('https://skeleton-soldier.online/manga/skeleton-soldier-couldnt-protect-the-dungeon-chapter-1-1/', 1)
-    downloadEveryChapter()
+    downloadEveryChapter(167) # value is hard coded, just change it here
     print('ALL DONE!')
 
     # testing stuff
